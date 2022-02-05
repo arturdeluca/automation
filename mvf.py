5f6b4bfa628d8b65907a29500f00b1b8434d689d
arturdeluca <67198234+arturdeluca@users.noreply.github.com>
2022-02-04 21:59:06 -0600

    Add files via upload

---
+++ mvf.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+
+srcPath = r'\Users\artur\Desktop\all\python_files\\'
+dstPath = r'\Users\artur\Desktop\all\python_files\basic_files\\'
+
+list = os.listdir(srcPath)
+
+for files in list:
+    source = srcPath + files
+    destination = dstPath + files
+    if files.startswith('1'):
+        shutil.move(source, destination)
+    elif files.startswith('2'):
+        shutil.move(source, destination)
+    elif files.startswith('3'):
+        shutil.move(source, destination)
+    elif files.startswith('4'):
+        shutil.move(source, destination)
+    elif files.startswith('5'):
+        shutil.move(source, destination)
+    elif files.startswith('6'):
+        shutil.move(source, destination)
+    elif files.startswith('7'):
+        shutil.move(source, destination)
+    elif files.startswith('8'):
+        shutil.move(source, destination)
+    elif files.startswith('9'):
+        shutil.move(source, destination)
